e5d8ba5b3623f0de9d185b053f4ac1faf76bb344
Desuoryuor <desuoryuora@gmail.com>
2023-10-22 13:33:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 /**
 * main - This Program Prints a line
 *
-*Return 1 means (Success)
+*Return: always (1)
+*
 */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
