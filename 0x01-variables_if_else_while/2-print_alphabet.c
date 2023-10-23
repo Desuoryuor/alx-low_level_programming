896919cd236d80e898fcdc5d8fafa118cac15c37
Desuoryuor <desuoryuora@gmail.com>
2023-10-23 16:29:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This code prints the Alphabets in Lowercase
+ * Return: Ale]ways 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
